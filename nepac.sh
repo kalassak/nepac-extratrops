0e41765d898904bf4f268a9ab52728d7fd96f6d0
Ubuntu <ubuntu@vps-9acc8a66.vps.ovh.us>
2025-01-27 06:18:55 +0000

    convert to py3, cartopy, add title & info

--- nepac.sh
+++ nepac.sh
@@ -1,2 +1,11 @@
-grads -blcx "run script.gs $1 $2"
-python2 nepac.py $1 $2
+date=$(date --date '5 hours ago' '+%Y%m%d')
+temphour=$(($(date --date '5 hours ago' '+%H') - $(($(date --date '5 hours ago' '+%H')%6))))
+
+printf -v hour '%02d' $temphour
+
+echo $date $hour
+
+grads -blcx "run script.gs $date $hour"
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate py3
+python nepac.py $date $hour
